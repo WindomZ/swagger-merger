7041211a27576a4289cb2da32035d41ee7f5ca26
WindomZ <git.windomz@gmail.com>
2017-04-12 08:31:06 +0800

    test: update transcode testing

--- test/transcoder.test.js
+++ test/transcoder.test.js
@@ -5,23 +5,27 @@ const test = require('ava')
 
 const transcoder = require('../lib/transcoder')
 
-test.serial('transcoder error 1', async (t) => {
+test('transcoder error', async (t) => {
   await transcoder({
     input: ''
   }).then(() => {
     t.fail('should be catch error')
   }).catch(() => t.pass())
-})
 
-test.serial('transcoder error 2', async (t) => {
   await transcoder({
-    input: './example/echo/swagger.xxx'
+    input: './example/echo/swagger.xxx',
+    output: './example/echo/swagger-trans.yaml'
   }).then(() => {
     t.fail('should be catch error')
   }).catch(() => t.pass())
-})
 
-test.serial('transcoder error 3', async (t) => {
+  await transcoder({
+    input: './example/echo/swagger.yaml',
+    output: './example/echo/swagger-trans.xxx'
+  }).then(() => {
+    t.fail('should be catch error')
+  }).catch(() => t.pass())
+
   await transcoder({
     input: './example/echo/echo.yaml',
     output: './example/echo/swagger-trans.yaml'
@@ -30,16 +34,14 @@ test.serial('transcoder error 3', async (t) => {
   }).catch(() => t.pass())
 })
 
-test.serial('transcoder pass 1', async (t) => {
+test('transcoder pass', async (t) => {
   await transcoder({
     input: './example/echo/echo.yaml',
     output: './example/echo/swagger-trans.json'
   }).then(() => {
     t.pass()
   }).catch(err => t.fail(err))
-})
 
-test.serial('transcoder pass 2', async (t) => {
   await transcoder({
     input: './example/echo/echo.json',
     output: './example/echo/swagger-trans.yaml'
