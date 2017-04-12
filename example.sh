5f8afd69202d52886e12169d96a838293cebbcbf
WindomZ <git.windomz@gmail.com>
2017-04-12 13:04:43 +0800

    add example petstore_simple

--- example.sh
+++ example.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-./bin/swagger-merger -i ./example/echo/index.yaml
-./bin/swagger-merger -i ./example/echo/index.json
+# example/echo
+#./bin/swagger-merger -i ./example/echo/index.yaml
+#./bin/swagger-merger -i ./example/echo/index.json
+#
+#./bin/swagger-merger transcode ./example/echo/swagger.json ./example/echo/swagger-trans.yaml
+#./bin/swagger-merger transcode ./example/echo/swagger.yaml ./example/echo/swagger-trans.json
 
-./bin/swagger-merger transcode ./example/echo/swagger.json ./example/echo/swagger-trans.yaml
-./bin/swagger-merger transcode ./example/echo/swagger.yaml ./example/echo/swagger-trans.json
+# example/petstore_simple
+./bin/swagger-merger -i ./example/petstore_simple/index.yaml
+#./bin/swagger-merger -i ./example/petstore_simple/index.json
+
+#./bin/swagger-merger transcode ./example/petstore_simple/swagger.json ./example/petstore_simple/swagger-trans.yaml
+#./bin/swagger-merger transcode ./example/petstore_simple/swagger.yaml ./example/petstore_simple/swagger-trans.json
