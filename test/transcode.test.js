e456fc404a8541fcc0e73c33b2210cbb72bc70b2
WindomZ <git.windomz@gmail.com>
2017-04-12 08:30:34 +0800

    test: add transcode testing

---
+++ test/transcode.test.js
@@ -0,0 +1,80 @@
+/**
+ * Created by WindomZ on 17-4-12.
+ */
+const test = require('ava')
+
+const {stringJSON2YAML, stringYAML2JSON} = require('../lib/transcode')
+
+test('transcode.stringJSON2YAML error', t => {
+  try {
+    stringJSON2YAML(null)
+    t.fail('should be catch error')
+  } catch (e) {
+    t.pass()
+  }
+
+  try {
+    stringJSON2YAML(1)
+    t.fail('should be catch error')
+  } catch (e) {
+    t.pass()
+  }
+
+  try {
+    stringJSON2YAML('')
+    t.fail('should be catch error')
+  } catch (e) {
+    t.pass()
+  }
+})
+
+test('transcode.stringJSON2YAML pass', t => {
+  try {
+    let result = stringJSON2YAML('{"a":1, "b":2}')
+    t.is(result, 'a: 1\nb: 2\n')
+    t.pass()
+  } catch (e) {
+    t.fail(e)
+  }
+
+  try {
+    let result = stringJSON2YAML({'a': 1, 'b': 2})
+    t.is(result, 'a: 1\nb: 2\n')
+    t.pass()
+  } catch (e) {
+    t.fail(e)
+  }
+})
+
+test('transcode.stringYAML2JSON error', t => {
+  try {
+    stringYAML2JSON(null)
+    t.fail('should be catch error')
+  } catch (e) {
+    t.pass()
+  }
+
+  try {
+    stringYAML2JSON(1)
+    t.fail('should be catch error')
+  } catch (e) {
+    t.pass()
+  }
+
+  try {
+    stringYAML2JSON('')
+    t.fail('should be catch error')
+  } catch (e) {
+    t.pass()
+  }
+})
+
+test('transcode.stringYAML2JSON pass', t => {
+  try {
+    let result = stringYAML2JSON('a: 1\nb: 2')
+    t.is(result, '{"a":1,"b":2}')
+    t.pass()
+  } catch (e) {
+    t.fail(e)
+  }
+})
