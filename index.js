9354f895ed63c63f7c2a77f8830975dc2054a5d8
WindomZ <git.windomz@gmail.com>
2017-05-25 18:03:20 +0800

    update version v1.0.2

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by WindomZ on 17-5-25.
+ */
+'use strict'
+
+module.exports = require('./lib/merger')
+
+module.exports.mergerJSON = require('./lib/merger_json')
+module.exports.mergerYAML = require('./lib/merger_yaml')
