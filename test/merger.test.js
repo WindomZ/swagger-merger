4d7991e5409e29ceebfca858910691f7002bb4f2
WindomZ <git.windomz@gmail.com>
2018-05-14 22:41:06 +0800

    test: add some testings without file extension name

--- test/merger.test.js
+++ test/merger.test.js
@@ -41,6 +41,16 @@ test.serial('merger pass', async (t) => {
   // whether the results are expected
   t.is('' + fs.readFileSync('./test/out.yaml'),
     '' + fs.readFileSync('./test/swagger.yaml'))
+
+  await merger({
+    input: './test/in.json'
+  }).then(() => {
+    t.pass()
+  }).catch(err => t.fail(err))
+
+  // whether the results are expected
+  t.is('' + fs.readFileSync('./test/out.json'),
+    '' + fs.readFileSync('./test/swagger.json'))
 })
 
 test.serial('merger heroku-pets pass', async (t) => {
