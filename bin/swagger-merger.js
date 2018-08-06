9551d129fc1474c0609219ed6a2ce312ba746084
Brinley Ang <brinley@unbolt.net>
2018-08-06 16:00:02 +1000

    Removed file param in program.action
    Seems like a recent change in commander has obsoleted that file parameter.

--- bin/swagger-merger.js
+++ bin/swagger-merger.js
@@ -20,7 +20,7 @@ program
     /^.+\.(json|yaml|yml)$/gi, null)
   .option('-c, --compact', 'compact JSON/YAML format string', null, null)
   .option('--debug', 'debug mode, such as print error tracks', null, null)
-  .action((file, options) => {
+  .action((options) => {
     noArgs = false
 
     merger({
